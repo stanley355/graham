42feec8fc5df495a9f6328cf5664134e64ded226
stanley <winatastanley355@gmail.com>
2022-08-01 09:33:52 +0700

    Initial web runner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
+use std::env;
+
+mod db;
+
+async fn serve_web(address: String, pool: db::PgPool) -> std::io::Result<()> {
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .route("/v1/", web::get().to(|| async { "Hello World!" }))
+    })
+    .bind(address)?
+    .run()
+    .await
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let host = &env::var("HOST").unwrap_or("127.0.0.1".to_string());
+    let port = &env::var("PORT").unwrap_or("8080".to_string());
+    let address = format!("{}:{}", host, port);
+
+    let pool = db::connect_pool();
+
+    serve_web(address, pool).await
 }
