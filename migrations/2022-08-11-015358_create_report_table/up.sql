7db62c96c4a67d00a2ba6a6e7dab3e189b166243
stanley <winatastanley355@gmail.com>
2022-08-11 09:13:54 +0700

    balance sheet migration

---
+++ migrations/2022-08-11-015358_create_report_table/up.sql
@@ -0,0 +1,21 @@
+-- Your SQL goes here
+CREATE TABLE balance (
+  id SERIAL PRIMARY KEY,
+  stock_id INTEGER NOT NULL,
+  cash BIGINT NOT NULL,
+  receivables BIGINT NOT NULL,
+  inventories BIGINT NOT NULL,
+  fixed_asset BIGINT NOT NULL,
+  quick_asset BIGINT NOT NULL,
+  current_asset BIGINT NOT NULL,
+  tangible_asset BIGINT NOT NULL,
+  st_liabilities BIGINT NOT NULL,
+  lt_liabilities BIGINT NOT NULL,
+  total_liabilities BIGINT NOT NULL,
+  net_cash_asset BIGINT NOT NULL,
+  net_quick_asset BIGINT NOT NULL,
+  net_current_asset BIGINT NOT NULL,
+  net_tangible_asset BIGINT NOT NULL,
+  share_outstanding BIGINT NOT NULL,
+  FOREIGN KEY (stock_id) REFERENCES stocks (id)
+);
--- src/schema.rs
+++ src/schema.rs
@@ -1,7 +1,36 @@
 table! {
+    balance (id) {
+        id -> Int4,
+        stock_id -> Int4,
+        cash -> Int8,
+        receivables -> Int8,
+        inventories -> Int8,
+        fixed_asset -> Int8,
+        quick_asset -> Int8,
+        current_asset -> Int8,
+        tangible_asset -> Int8,
+        st_liabilities -> Int8,
+        lt_liabilities -> Int8,
+        total_liabilities -> Int8,
+        net_cash_asset -> Int8,
+        net_quick_asset -> Int8,
+        net_current_asset -> Int8,
+        net_tangible_asset -> Int8,
+        share_outstanding -> Int8,
+    }
+}
+
+table! {
     stocks (id) {
         id -> Int4,
         code -> Varchar,
         name -> Varchar,
     }
 }
+
+joinable!(balance -> stocks (stock_id));
+
+allow_tables_to_appear_in_same_query!(
+    balance,
+    stocks,
+);
