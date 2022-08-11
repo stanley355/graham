95fc7cceb02163469e8d8dbdfc056fb106fa2b64
stanley <winatastanley355@gmail.com>
2022-08-11 08:50:19 +0700

    ENdpoint: view all stocks

--- src/stock/handler.rs
+++ src/stock/handler.rs
@@ -1,6 +1,6 @@
 use crate::db::PgPool;
 use crate::stock::{model, req};
-use actix_web::{post, web, HttpResponse};
+use actix_web::{get, post, web, HttpResponse};
 
 #[post("/")]
 async fn add_stock(pool: web::Data<PgPool>, body: web::Json<req::AddStockReq>) -> HttpResponse {
@@ -16,7 +16,15 @@ async fn add_stock(pool: web::Data<PgPool>, body: web::Json<req::AddStockReq>) -
     }
 }
 
+#[get("/")]
+async fn view_stocks(pool: web::Data<PgPool>) -> HttpResponse {
+    match model::Stock::view_all(pool) {
+        Ok(all_stocks) => HttpResponse::Ok().json(all_stocks),
+        Err(err) => HttpResponse::InternalServerError().body(format!("Error {:?}", err)),
+    }
+}
+
 // Routing for hosts
 pub fn route(config: &mut web::ServiceConfig) {
-    config.service(add_stock);
+    config.service(add_stock).service(view_stocks);
 }
--- src/stock/model.rs
+++ src/stock/model.rs
@@ -1,5 +1,5 @@
 use crate::db::PgPool;
-use crate::schema::stocks::dsl::*;
+use crate::schema::stocks::*;
 use crate::stock::req;
 
 use actix_web::web;
@@ -16,15 +16,20 @@ pub struct Stock {
 impl Stock {
     pub fn check_existence(pool: web::Data<PgPool>, stock_code: String) -> QueryResult<bool> {
         let conn = &pool.get().unwrap();
-        let exist = select(exists(stocks.filter(code.eq(stock_code.clone())))).get_result(conn);
-
-        exist
+        select(exists(dsl::stocks.filter(code.eq(stock_code.clone())))).get_result(conn)
     }
 
     pub fn add(pool: web::Data<PgPool>, body: web::Json<req::AddStockReq>) -> QueryResult<Stock> {
         let conn = &pool.get().unwrap();
         let data = (&code.eq(&body.code), &name.eq(&body.name));
 
-        diesel::insert_into(stocks).values(data).get_result(conn)
+        diesel::insert_into(dsl::stocks)
+            .values(data)
+            .get_result(conn)
+    }
+
+    pub fn view_all(pool: web::Data<PgPool>) -> QueryResult<Vec<Stock>> {
+        let conn = &pool.get().unwrap();
+        table.load::<Stock>(conn)
     }
 }
