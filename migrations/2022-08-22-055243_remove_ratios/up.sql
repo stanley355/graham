c2c634b4a66c788a169befdebe634f3c5f5f672b
stanley <winatastanley355@gmail.com>
2022-08-22 12:53:53 +0700

    remove ratios table

---
+++ migrations/2022-08-22-055243_remove_ratios/up.sql
@@ -0,0 +1,3 @@
+-- Your SQL goes here
+DROP TABLE per_share_ratios;
+DROP TABLE comparative_ratios;
--- src/schema.rs
+++ src/schema.rs
@@ -22,24 +22,6 @@ table! {
 }
 
 table! {
-    comparative_ratios (id) {
-        id -> Int4,
-        stock_id -> Int4,
-        year -> Int4,
-        gross_profit_margin -> Int4,
-        operating_profit_margin -> Int4,
-        net_profit_margin -> Int4,
-        current_asset_return -> Int4,
-        tangible_asset_return -> Int4,
-        total_liability_return -> Int4,
-        revenue_receivable_return -> Int4,
-        revenue_inventory_return -> Int4,
-        current_asset_liabilities_return -> Int4,
-        tangible_asset_total_liabilities_return -> Int4,
-    }
-}
-
-table! {
     income (id) {
         id -> Int4,
         stock_id -> Int4,
@@ -57,22 +39,6 @@ table! {
 }
 
 table! {
-    per_share_ratios (id) {
-        id -> Int4,
-        stock_id -> Int4,
-        year -> Int4,
-        cash_equity -> Nullable<Int8>,
-        quick_equity -> Nullable<Int8>,
-        current_equity -> Nullable<Int8>,
-        tangible_equity -> Nullable<Int8>,
-        gross_profit -> Nullable<Int8>,
-        operating_profit -> Nullable<Int8>,
-        net_profit -> Nullable<Int8>,
-        cashflow -> Nullable<Int8>,
-    }
-}
-
-table! {
     stocks (id) {
         id -> Int4,
         code -> Varchar,
@@ -81,14 +47,10 @@ table! {
 }
 
 joinable!(balance -> stocks (stock_id));
-joinable!(comparative_ratios -> stocks (stock_id));
 joinable!(income -> stocks (stock_id));
-joinable!(per_share_ratios -> stocks (stock_id));
 
 allow_tables_to_appear_in_same_query!(
     balance,
-    comparative_ratios,
     income,
-    per_share_ratios,
     stocks,
 );
