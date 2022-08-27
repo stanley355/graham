ed7fae6ed7a40bcf910cc837ffa087ee8135093e
Stanley Winata <winatastanley355@gmail.com>
2022-08-27 20:26:27 +0700

    Add analysis handler

--- src/report/handler.rs
+++ src/report/handler.rs
@@ -1,3 +1,4 @@
+use crate::analysis::model::Analysis;
 use crate::db::PgPool;
 use crate::report::{model::*, req::ReportParam};
 use crate::stock::model::Stock;
@@ -49,6 +50,38 @@ async fn view_reports(pool: web::Data<PgPool>, param: web::Query<ReportParam>) -
     }
 }
 
+#[get("/analysis/")]
+async fn view_analysis(pool: web::Data<PgPool>, param: web::Query<ReportParam>) -> HttpResponse {
+    match (param.code.clone(), param.year) {
+        (Some(code), Some(year)) => {
+            let stock_id = Stock::get_id(pool.clone(), code);
+
+            match stock_id {
+                Ok(id) => {
+                    let identifier = ReportIdentifier {
+                        stock_id: id,
+                        year: year,
+                    };
+
+                    let report_result = Report::get_report(pool, identifier);
+
+                    match report_result {
+                        Ok(report) => {
+                            let analysis = Analysis::new(report);
+                            HttpResponse::Ok().json(analysis)
+                        }
+                        Err(err) => {
+                            HttpResponse::InternalServerError().body(format!("Error {:?}", err))
+                        }
+                    }
+                }
+                Err(err) => HttpResponse::BadRequest().body(format!("Error {:?}", err)),
+            }
+        }
+        _ => HttpResponse::BadRequest().body(format!("Missing Parameter: code, year")),
+    }
+}
+
 pub fn route(config: &mut web::ServiceConfig) {
-    config.service(view_reports);
+    config.service(view_reports).service(view_analysis);
 }
